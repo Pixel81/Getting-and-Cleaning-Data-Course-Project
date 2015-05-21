9c5d2c3f5fbf333b535213c80dbe83a6b0f35896
Pixel81 <pat201281@gmail.com>
2015-05-21 22:00:14 +0200

    First commit
    First commit on script

---
+++ run_analysis.R
@@ -0,0 +1,18 @@
+# Script for Getting & cleaning course
+# by Pixel - 2015.05.05
+
+# Works with the Dataset.zip files
+
+# 1. Merges the training and the test sets to create one data set
+
+# 2. Extracts only the measurements on the mean and standard 
+#    deviation for each measurements
+
+# 3. Uses descriptive activity names to name the activities in
+#    the data set
+
+# 4. Appropriately labels the data set with descriptive variable
+#    names
+
+# 5. From step-4, creates a second, independant tidy data set
+#    with the average of each variable for each activity
